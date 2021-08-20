80cfeff9e23ef5921ff1dc3650ed0f39db575f09
Andrew Gonzalez <andrewgonzo23@gmail.com>
2021-08-20 01:12:55 -0500

    update

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Dotfiles
 
 My dotfiles.
+
+## Installation
+
+```
+$ git clone https://github.com/GonzalezAndrew/dotfiles.git
+
+$ cd dotfiles
+
+$ ./install.sh update
+Updating dotfiles at /Users/andrewgonzalez...
+bash or zsh? (bash): bash
+Configuring for bash
+linking shared dotfiles
+done... :-)
+```
--- install.sh
+++ install.sh
@@ -1,24 +1,42 @@
 #!/bin/sh
 set -e
 
-shell="bash"
-printf "bash or zsh? (%s): " "${shell}"
-read -r val 
-test -n "${val}" && shell="${val}"
+_update() {
+    echo "Updating dotfiles at $HOME..."
 
-if [ $shell = "bash" ]; then
-    ln -sf "$PWD/.bashrc" "$HOME/.bashrc"
-    ln -sf "$PWD/.profile" "$HOME/.profile"
-    ln -sf "$PWD/.profile" "$HOME/.bash_profile"
-else
-    ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
-    ln -sf "$PWD/.zsh" "$HOME/.zsh"
+    shell="bash"
+    printf "bash or zsh? (%s): " "${shell}"
+    read -r val
+    test -n "${val}" && shell="${val}"
 
-fi 
+    if [ $shell = "bash" ]; then
+        echo 'Configuring for bash'
+        ln -sf "$PWD/.bashrc" "$HOME/.bashrc"
+        ln -sf "$PWD/.profile" "$HOME/.profile"
+        ln -sf "$PWD/.profile" "$HOME/.bash_profile"
+    else
+        echo 'Configuring for zsh'
+        ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
+        ln -sf "$PWD/.zsh" "$HOME/.zsh"
 
-ln -sf "$PWD/.dircolors" "$HOME/.dircolors"
-ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$PWD/.config" "$HOME/.config"
-ln -sf "$PWD/.aws" "$HOME/.aws"
-ln -sf "$PWD/.functions" "$HOME/.functions"
-mkdir -p "$HOME/.local/bin" || true
+    fi
+
+    echo 'linking shared dotfiles'
+    ln -sf "$PWD/.dircolors" "$HOME/.dircolors"
+    ln -sf "$PWD/.tmux.conf" "$HOME/.tmux.conf"
+    ln -sf "$PWD/.config" "$HOME/.config"
+    ln -sf "$PWD/.aws" "$HOME/.aws"
+    ln -sf "$PWD/.functions" "$HOME/.functions"
+    mkdir -p "$HOME/.local/bin" || true
+
+    echo 'done... :-)'
+}
+
+case "$1" in
+update)
+    _update
+    ;;
+*)
+    echo "Usage: $0 { update }"
+    ;;
+esac
