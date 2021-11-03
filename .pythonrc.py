7d53421f9a996fdd280e066ff0ee54ab180926ed
Andrew Gonzalez <andrewgonzo23@gmail.com>
2021-11-03 09:43:51 -0500

    Create .pythonrc.py

---
+++ .pythonrc.py
@@ -0,0 +1,19 @@
+import readline
+import rlcompleter
+import os 
+
+try:
+    # install rich for repl
+    from rich import pretty
+    pretty.install()
+except ImportError:
+    pass
+
+# Tab Completion
+readline.parse_and_bind("tab: complete")
+del readline, rlcompleter
+
+
+# Change exit() to exit
+type(exit).__repr__ = lambda self: self()
+
