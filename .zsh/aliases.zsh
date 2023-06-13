9556c9d5706c22489d8cd6036136a7ca045898a1
Andy <andrewgonzo23@gmail.com>
2023-06-13 11:31:22 -0500

    Update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -45,3 +45,48 @@ alias path='echo -e ${PATH//:/\\n}'
 
 
 alias fish='asciiquarium'
+
+## cd into github workspace for work or personal
+workspace() {
+    profile=$(gh profile show)
+
+    case "$1" in
+    personal | me | self)
+        if [ "$profile" != "GonzalezAndrew" ]; then
+            gh profile switch GonzalezAndrew
+        fi
+        cd "$HOME"/github.com/GonzalezAndrew || {
+            echo "Failed to cd to personal workspace"
+        }
+        ;;
+    work)
+        if [ "$profile" != "Andy-Gonzalez" ]; then
+            gh profile switch Andy-Gonzalez
+        fi
+        cd "$HOME"/github.com/Andy-Gonzalez || {
+            echo "Failed to cd to work workspace"
+        }
+        ;;
+    *)
+        echo "Usage: workspace [personal | work]"
+        ;;
+    esac
+}
+
+## merge all pre-commit.ci pull requests
+pre-ci-auto() {
+    prs=$(GH_PAGER="" gh search prs \
+        --state open \
+        --owner GonzalezAndrew \
+        --json author,repository,number \
+        --jq 'map(select(.author.login|contains("pre-commit-ci[bot]")))')
+
+    for row in $(echo "$prs" | jq -r '.[] | @base64'); do
+        _jq() {
+            echo "${row}" | base64 --decode | jq -r "${1}"
+        }
+        pr_number=$(_jq '.number')
+        repo=$(_jq '.repository.nameWithOwner')
+        gh pr merge "$pr_number" -R "$repo" --auto -d -m
+    done
+}
